ae09776521bac93650ac0d7bab114bbb6f5e21ff
ibrahimgulbutt <ibrahimgulbutt242@gmail.com>
2026-03-24 15:41:44 +0500

    config changed

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_URL
-  ? 'https://backend-django-production-004e.up.railway.app'
-  : 'http://127.0.0.1:8000';
+export const BASE_URL = import.meta.env.VITE_API_URL 
+  || 'https://backend-django-production-004e.up.railway.app';
 
 export const API_BASE_URL = `${BASE_URL}/api`;
